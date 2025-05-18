0629afe1fa63b1e261d5ddfabc963668b1549133
Karol Pietrów <73913259+KarolPietrow@users.noreply.github.com>
2025-05-19 00:39:55 +0200

    Delete src directory

--- src/Student.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class Student extends User {
-    public Student(String id, String name, String login, String password) {
-        super(id, name, login, password);
-    }
-}
--- src/Teacher.java
+++
@@ -1,5 +0,0 @@
-public class Teacher extends User {
-    Teacher(String id, String name, String login, String password) {
-        super(id, name, login, password);
-    }
-}
